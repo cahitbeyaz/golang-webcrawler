1883581159ba9229c99b0a74b1ad7a775b869aab
beyazc <cahitbyz@gmail.com>
2017-11-28 09:38:31 +0300

    fix for dublicate search for records with result 'not found'

--- main.go
+++ main.go
@@ -36,14 +36,14 @@ func Crawl(url string, depth int, fetcher Fetcher) {
 	}
 
 	body, urls, err := fetcher.Fetch(url)
+	visitedUrls.Lock()
+	visitedUrls.urls[url] = true
+	visitedUrls.Unlock()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Printf("found: %s %q\n", url, body)
-	visitedUrls.Lock()
-	visitedUrls.urls[url] = true
-	visitedUrls.Unlock()
 	for _, u := range urls {
 		Crawl(u, depth-1, fetcher)
 	}
@@ -51,36 +51,48 @@ func Crawl(url string, depth int, fetcher Fetcher) {
 }
 
 func main() {
+	populateFakeFatcherStruct()
 	Crawl("http://golang.org/", 4, fetcher)
 }
 
 // fakeFetcher is Fetcher that returns canned results.
-type fakeFetcher map[string]*fakeResult
+type fakeFetcherStruct struct {
+	fakeFetcher map[string]*fakeResult
+	sync.RWMutex
+}
 
 type fakeResult struct {
 	body string
 	urls []string
 }
 
-func (f fakeFetcher) Fetch(url string) (string, []string, error) {
-	if res, ok := f[url]; ok {
+func (f fakeFetcherStruct) Fetch(url string) (string, []string, error) {
+
+	f.RLock()
+	res, ok := f.fakeFetcher[url]
+	f.RUnlock()
+
+	if ok {
 		return res.body, res.urls, nil
 	}
+
 	return "", nil, fmt.Errorf("not found: %s", url)
 }
 
-var anotherFetcher = fakeFetcher{}
-
 // fetcher is a populated fakeFetcher.
-var fetcher = fakeFetcher{
-	"http://golang.org/": &fakeResult{
-		"The Go Programming Language",
+var fetcher = fakeFetcherStruct{}
+
+func populateFakeFatcherStruct() {
+	fetcher.fakeFetcher = make(map[string]*fakeResult)
+
+	fetcher.fakeFetcher["http://golang.org/"] = &fakeResult{"The Go Programming Language",
 		[]string{
 			"http://golang.org/pkg/",
 			"http://golang.org/cmd/",
 		},
-	},
-	"http://golang.org/pkg/": &fakeResult{
+	}
+
+	fetcher.fakeFetcher["http://golang.org/pkg/"] = &fakeResult{
 		"Packages",
 		[]string{
 			"http://golang.org/",
@@ -88,19 +100,21 @@ var fetcher = fakeFetcher{
 			"http://golang.org/pkg/fmt/",
 			"http://golang.org/pkg/os/",
 		},
-	},
-	"http://golang.org/pkg/fmt/": &fakeResult{
+	}
+
+	fetcher.fakeFetcher["http://golang.org/pkg/fmt/"] = &fakeResult{
 		"Package fmt",
 		[]string{
 			"http://golang.org/",
 			"http://golang.org/pkg/",
 		},
-	},
-	"http://golang.org/pkg/os/": &fakeResult{
+	}
+
+	fetcher.fakeFetcher["http://golang.org/pkg/os/"] = &fakeResult{
 		"Package os",
 		[]string{
 			"http://golang.org/",
 			"http://golang.org/pkg/",
 		},
-	},
+	}
 }
